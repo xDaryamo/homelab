6ad8b3a61201b9e92cecd995ceb85137fba52f8a
xDaryamo <mr.dario.mazza@gmail.com>
2026-02-04 15:54:25 +0000

    fix(oauth2-proxy): add required resource access for Microsoft Graph API

--- terraform/stacks/security/oauth2_proxy_ad.tf
+++ terraform/stacks/security/oauth2_proxy_ad.tf
@@ -11,6 +11,15 @@ resource "azuread_application" "oauth2_proxy" {
   }
 
   group_membership_claims = ["SecurityGroup"]
+
+  required_resource_access {
+    resource_app_id = "00000003-0000-0000-c000-000000000000" # Microsoft Graph
+
+    resource_access {
+      id   = "e1fe6dd8-ba31-4d61-89e7-88639da4683d" # User.Read
+      type = "Scope"
+    }
+  }
 }
 
 resource "azuread_service_principal" "oauth2_proxy" {
